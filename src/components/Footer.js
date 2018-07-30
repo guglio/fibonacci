4f5e7af6354a78a9d2416d42f9a380c2d5c39e81
Guglielmo Turco <guglielmo.turco@gmail.com>
2018-07-29 22:13:33 -0700

    Moved Footer to his folder with corresponding css

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer>
-    Footer test
-  </footer>
-);
-
-export default Footer;
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Footer.css';
+
+const Footer = () => (
+  <footer className="footer">
+    <p className="credits">Created by <a href="//guglielmo-turco.com" target="_blank" rel="noopener noreferrer">Guglielmo Turco</a></p>
+  </footer>
+);
+
+export default Footer;
