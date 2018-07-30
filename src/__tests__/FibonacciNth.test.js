9a0ce5a57d6edfb7ddb6f7a1838bfe49077c5921
Guglielmo <guglielmo.turco@allscripts.com>
2018-07-30 13:58:28 -0700

    Add reverse test of the formula

--- src/__tests__/FibonacciNth.test.js
+++ src/__tests__/FibonacciNth.test.js
@@ -12,7 +12,16 @@ describe('FibonacciNth', () => {
   it('num > 2; FibonacciNth(num) = integer', () => {
     let testArr = [0,1,1,2,3,5,8,13,21,34,55];
 
-    for(let i=2;i<10;i++)
+    for ( let i=2; i<10; i++ )
       expect(`${FibonacciNth(i)}`).toBe(`${testArr[i]}`);
+  });
+  it('reverse test the formula', () => {
+    let testArr = [];
+    let n = 10;
+    for ( let i = 0; i <= n; i++)
+      testArr.push(parseInt(`${FibonacciNth(i)}`,10));
+    for ( let i = testArr.length - 1; i > 1; i-- ){
+      expect(testArr[i] - testArr[i-1]).toBe(testArr[i-2]);
+    }
   });
 });
