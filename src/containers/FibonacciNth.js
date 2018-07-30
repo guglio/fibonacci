2072ec03ec67ad72dfe391b6e70c8e9c3e6fd4c1
Guglielmo Turco <guglielmo.turco@gmail.com>
2018-07-29 21:40:29 -0700

    Parse the input to integer

--- src/containers/FibonacciNth.js
+++ src/containers/FibonacciNth.js
@@ -1,14 +1,16 @@
-const FibonacciNth = ( num = 2 ) => {
+const FibonacciNth = ( n = 2 ) => {
+  var num = parseInt(n,10);
   if(num < 2){
     return "Enter a number >= 2";
   }
-
+  if(!Number.isInteger(num)){
+    return "Not an integer";
+  }
   let [curr, next] = [1, 0];
   while (num-- > 0) {
     [curr, next] = [curr + next, curr];
   }
   return next;
 };
-
 
 export default FibonacciNth;
