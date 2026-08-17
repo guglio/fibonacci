254c3b7b5cd8dafc674fb3bd263ea2f21d581933
guglio <guglio@users.noreply.github.com>
2026-08-17 16:39:01 -0700

    feat: add proper BigNumber types to useFibonacciNth hook

--- src/utils/useFibonacciNth.ts
+++ src/utils/useFibonacciNth.ts
@@ -1,10 +1,12 @@
 import { create, all } from "mathjs";
+import type { BigNumber } from "mathjs";
 const mathjs = create(all);
 mathjs.config({ number: "BigNumber" });
 
 export const useFibonacciNth = (num = 2) => {
   // calculate the nth on the Fibonacci sequence with iterable object
-  let [curr, prev] = [1, 0];
+  let curr: BigNumber = mathjs.bignumber(1);
+  let prev: BigNumber = mathjs.bignumber(0);
   while (num-- > 0) {
     // I used math.js library to handle bignumbers (if n > 1477, the result was Infinity)
     // the following formula is equal to:
