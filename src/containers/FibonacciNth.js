48d1b4eef5786d922a9bcadb982576a485677a5c
Guglielmo Turco <5477883+guglio@users.noreply.github.com>
2026-08-17 10:49:35 -0700

    Fixed mathjs import and configuration

--- src/containers/FibonacciNth.js
+++ src/containers/FibonacciNth.js
@@ -1,18 +1,23 @@
-import * as math from 'mathjs'; // handle the BigNumber
+import { create, all } from 'mathjs';
 
-math.config({
+const mathjs = create(all);
+
+mathjs.config({
   number: 'BigNumber', // Set type of number to be 'BigNumber'
-  precision: 20 // Number of significant digits for BigNumbers
-})
+  precision: 20, // Number of significant digits for BigNumbers
+});
 
-const FibonacciNth = ( num = 2 ) => {
+const FibonacciNth = (num = 2) => {
   // calculate the nth on the Fibonacci sequence with iterable object
   let [curr, prev] = [1, 0];
   while (num-- > 0) {
     // I used math.js library to handle bignumbers (if n > 1477, the result was Infinity)
     // the following formula is equal to:
     // [curr, prev] = [curr + prev, curr];
-    [curr, prev] = [math.add(math.bignumber(curr), math.bignumber(prev)), math.bignumber(curr)];
+    [curr, prev] = [
+      mathjs.add(mathjs.bignumber(curr), mathjs.bignumber(prev)),
+      mathjs.bignumber(curr),
+    ];
   }
   return prev;
 };
