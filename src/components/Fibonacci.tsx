00d395569c375b3df448f158e649cccbd01ed530
guglio <guglio@users.noreply.github.com>
2026-08-17 17:23:30 -0700

    styling: add yellow background to container, remove from input, increase spacing

--- src/components/Fibonacci.tsx
+++ src/components/Fibonacci.tsx
@@ -21,51 +21,51 @@ const Fibonacci = () => {
   };
 
   return (
-    <div className='fibonacci'>
-      <div id='formFibonacci' className='space-y-6'>
-        <input
-          id='fibonacciNum'
-          className='input input-number h-12 bg-yellow-100'
-          value={fibonacciN ?? ''}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            const value = e.target.value;
-            if (value === '') {
-              setFibonacciN(undefined);
-            } else {
-              const num = Number(value);
-              if (!isNaN(num)) {
-                setFibonacciN(num);
-              }
-              // if NaN, do nothing (keep previous state)
-            }
-          }}
-          autoComplete='off'
-          placeholder={'2'}
-        />
-        <div className='btn-group flex space-x-6 mt-6'>
-          <button
-            type='submit'
-            id='submit'
-            className='btn btn-submit px-6 py-3'
-            disabled={calculateDisable()}
-            onClick={() => calculateFibonacci()}
-          >
-            Calculate
-          </button>
-          <button
-            type='reset'
-            id='reset'
-            className='btn btn-reset px-6 py-3'
-            onClick={() => resetForm()}
-          >
-            Reset
-          </button>
-        </div>
-      </div>
-      <div id='FibonacciNth' className='result'>
-        <p>{message}</p>
-      </div>
-    </div>
+    <div className='fibonacci bg-yellow-100 min-h-screen p-4'>
+     <div id='formFibonacci' className='space-y-6'>
+       <input
+         id='fibonacciNum'
+         className='input input-number h-12'
+         value={fibonacciN ?? ''}
+         onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+           const value = e.target.value;
+           if (value === '') {
+             setFibonacciN(undefined);
+           } else {
+             const num = Number(value);
+             if (!isNaN(num)) {
+               setFibonacciN(num);
+             }
+             // if NaN, do nothing (keep previous state)
+           }
+         }}
+         autoComplete='off'
+         placeholder={'2'}
+       />
+       <div className='btn-group flex space-x-6 mt-6'>
+         <button
+           type='submit'
+           id='submit'
+           className='btn btn-submit px-6 py-3'
+           disabled={calculateDisable()}
+           onClick={() => calculateFibonacci()}
+         >
+           Calculate
+         </button>
+         <button
+           type='reset'
+           id='reset'
+           className='btn btn-reset px-6 py-3'
+           onClick={() => resetForm()}
+         >
+           Reset
+         </button>
+       </div>
+     </div>
+     <div id='FibonacciNth' className='result'>
+       <p>{message}</p>
+     </div>
+   </div>
   );
 };
 
